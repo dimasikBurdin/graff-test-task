5cd0c99e75f5a0cd2aa0d9f441f7952c022d5b09
dimasikBurdin <mityabur@gmail.com>
2022-07-18 19:40:58 +0500

    добавлен и кастомизирован календарь

--- src/components/mainPart/mainPart.tsx
+++ src/components/mainPart/mainPart.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import './mainPart.css';
 import backImage from './mainImage.svg';
 import whiteLogo from './logoWhiteIcon.svg';
+import Calendar from "react-calendar";
+import './rightBar/calendar.css';
 
 export const MainPart:React.FC = React.memo(() => {
+
+    function onClickEntry() {
+
+    }
+
     return <div className="main-part-container">
         <div className="main-part-image"></div>
         <img className="main-part-logo" src={whiteLogo} alt="лого компании" />
@@ -17,7 +24,15 @@ export const MainPart:React.FC = React.memo(() => {
                 </span>
 
             </div>
-            <button className="main-part-info-button">Записаться на просмотр</button>
+            <button className="main-part-info-button" onClick={() => onClickEntry()}>Записаться на просмотр</button>
         </div>
+        <Calendar 
+            showNeighboringMonth={false}
+            showDoubleView={false}
+            next2Label={null}
+            prev2Label={null}
+            tileDisabled={(a) => a.date.getDate() === 23}
+            minDate={new Date()}
+        />
     </div>
 })
---
+++ src/components/mainPart/rightBar/rightBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './calendar.css';
+import './rightBar.css';
+
+export const RightBar:React.FC = React.memo(() => {
+    return <div className="right-bar-container">
+        
+    </div>
+})
